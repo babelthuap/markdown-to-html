e58d1e4dcad9e5bfa81e0d503fecd4a45b250023
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-11-09 20:51:28 -0800

    POST markdown from client to server and return that same markdown string

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
+
 var app = express();
 app.set('view engine', 'jade');
 
 
 
 // enable POST body parsing
-// app.use(bodyParser.urlencoded({ extended: true })); // form data - for webpage
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // form data - for webpage
+// app.use(bodyParser.json());
 
 // log in the console
 app.use(morgan('combined'));
@@ -26,6 +27,13 @@ app.use(express.static('public'));
 // display the index page when a user accessess the server from a browser
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+// receive markdown, convert it to HTML, respond with resulting HTML
+app.post('/markdown', function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  console.log('POST REQUEST:', req);
+  res.send('not converted yet: ' + req.body.string);
 });
 
 
--- public/main.js
+++ public/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('working');
+$(document).ready(() => {
+  
+  $('#convert').click(convert);
+
+  function convert() {
+    let md = $('#md').val();
+
+    $.post('/markdown', {string: md})
+    .done(displayHTML)
+    .fail((err) => console.log("ERROR:", err));
+  }
+
+  function displayHTML(data) {
+    $('#rendered').text(data);
+  }
+
+});
